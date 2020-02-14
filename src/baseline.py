2754dd4b2cc8767a83ac8ef376a00a3e1ac5a161
David Whisler <dwhis428@gmail.com>
2020-02-14 11:08:02 -0800

    compile data extraction code

--- src/baseline.py
+++ src/baseline.py
@@ -1,10 +1,49 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
-def read_csv_data(filename):
-    data = pd.read_csv(filename)
-    print(data)
+import sklearn
+from sklearn import svm, neural_network, naive_bayes, ensemble, neighbors, datasets
+from sklearn.model_selection import train_test_split, cross_val_score, StratifiedKFold
+from sklearn.metrics import accuracy_score
+
+from data_extraction import get_loan_data
+
+
+def run_baseline_classifiers(data, target):
+    #Includes a sample of several the machine learning classifiers
+    classifiers = [
+        # ensemble.RandomForestClassifier(random_state=0, n_estimators=10),
+        # svm.SVC(gamma='auto'),
+        # naive_bayes.GaussianNB(priors=None),
+        # neighbors.KNeighborsClassifier(n_neighbors=5),
+        neural_network.MLPClassifier(activation='relu', solver='adam', alpha=1e-5, hidden_layer_sizes=(100,30,), random_state=0, learning_rate_init=0.0001),
+        ]
+    features_train, features_test, labels_train, labels_test = train_test_split(data, target, test_size=0.1, random_state=5)
+
+
+    for clf in classifiers:
+        print(clf.__class__.__name__)
+        #Parameters used in creating this classifier
+        print('Parameters: ' + str(clf.get_params()))
+        #Train & predict classifier
+        clf.fit(features_train, labels_train)
+
+        #Cross validation
+        scores = cross_val_score(clf, features_train, labels_train, cv=5)
+        print('Cross Validation Scores: ' + str(scores))
+        print('Avg Accuracy: %0.2f (+/- %0.2f)' % (scores.mean(), scores.std() * 2))
+
+        #Test accuracy
+        # train_accuracy = accuracy_score(labels_train, clf.predict(features_train))
+        # print("Training accuracy: ", train_accuracy)
+        test_accuracy = accuracy_score(labels_test, clf.predict(features_test))
+        print("Test accuracy: ", test_accuracy)
 
 if __name__ == '__main__':
-    filename = "loan.csv"
-    read_csv_data()
+    #X, y = datasets.load_iris(return_X_y=True)
+    filename = "../data/accepted_2007_to_2018Q4.csv"
+    cols_filename = "cols_all_test.txt"
+    X, y = get_loan_data(filename, cols_filename, 10000)
+
+    run_baseline_classifiers(X, y)
---
+++ src/data_extraction.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import numpy as np
+
+
+def sampling_k_elements(group, k):
+    if len(group) < k:
+        return group
+    return group.sample(k)
+
+def get_loan_data(filename, cols_filename, n):
+    df = pd.read_csv(filename, nrows=n)
+
+    df = df.loc[df['loan_status'].isin(['Default', 'Fully Paid', 'Charged Off'])] #only use datapoints with relevant labels
+    label_map = {'Default': 1, 'Charged Off': 1,'Fully Paid':0}
+    df = df.applymap(lambda s: label_map.get(s) if s in label_map else s)
+    df = df.groupby('loan_status').apply(sampling_k_elements, min(df['loan_status'].value_counts())).reset_index(drop=True) # balance dataset in labels
+    labels = df['loan_status']
+
+    with open(cols_filename, 'r') as f:
+        cols = f.readlines()
+        cols = [col.strip() for col in cols]
+    df = df[cols]
+
+    emp_length_map = {'< 1 year': 0, '1 year': 1,'2 years':2,'3 years':3,'4 years':4,'5 years':5,'6 years':6,'7 years':7,'8 years':8,'9 years':9, '10 years':10,'10+ years':11}
+    df = df.applymap(lambda s: emp_length_map.get(s) if s in emp_length_map else s)
+
+    df = df.fillna(0)
+
+    with open("cols_onehot.txt", 'r') as f:
+        cols_onehot = f.readlines()
+        cols_onehot = [col.strip() for col in cols_onehot]
+
+    one_hot = pd.get_dummies(df[cols_onehot])
+    df = df.join(one_hot)
+    df = df.drop(columns=cols_onehot)
+
+    return df, labels
+
+
+if __name__ == '__main__':
+    filename = "../data/accepted_2007_to_2018Q4.csv"
+    cols_filename = "cols_all_test.txt"
+    X, y = get_loan_data(filename, cols_filename, 10000)
+
+    print(y.value_counts())
